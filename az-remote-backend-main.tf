b76cebdf66ddc0cef96ca14093174b3d5c0e86b2
Alessio Mancini <alessio.mancini@aesys.tech>
2022-05-11 18:06:28 +0200

    Added file az-remote-backend-output.tf and modified file az-remote-backend-main.tf and az-remote-backend-variables.tf

--- az-remote-backend-main.tf
+++ az-remote-backend-main.tf
@@ -1,8 +1,47 @@
 # Generate a random storage name
 resource "random_string" "tf-name" {
-    length = 8
-    upper = false
-    number = true
-    lower = true
-    special = false  
+  length  = 8
+  upper   = false
+  number  = true
+  lower   = true
+  special = false
+}
+
+# Create a resource group for the Terraform State File
+
+data "azurerm_resources" "resource-group" {
+  resource_group_name = "RGOrange-Test"
+}
+
+# Create a Storage Accont for the Terraform State File
+
+resource "azurerm_storage_account" "state-sta" {
+  depends_on = [data.azurerm_resources.resource-group]
+
+  name                      = "${lower(var.user)}tf${random_string.tf-name.result}}"
+  resource_group_name       = data.azurerm_resources.resource-group
+  location                  = data.azurerm_resources.resource-group.location
+  account_kind              = "StorageV2"
+  account_tier              = "Standard"
+  access_tier               = "Hot"
+  account_replication_type  = "ZRS"
+  enable_https_traffic_only = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Utilizzatore = var.user
+  }
+
+}
+
+# Create a Storage Container for the Core State File
+
+resource "azurerm_storage_container" "terraform-container" {
+  depends_on = [azurerm_storage_account.state-sta]
+
+  name                 = "terraform-tfstate"
+  storage_account_name = azurerm_storage_account.state-sta.name
 }
--- az-remote-backend-variables.tf
+++ az-remote-backend-variables.tf
@@ -1,17 +1,24 @@
 variable "company" {
-    type = string
-    description = "This variable defines the name of the company"  
+  type        = string
+  description = "This variable defines the name of the company"
 }
 
 variable "environment" {
-    type = string
-    description = "This variable defines the environment to be built"
-  
+  type        = string
+  description = "This variable defines the environment to be built"
+
+}
+
+variable "user" {
+  type        = string
+  description = "This variable defines the user"
+  default     = "alessio.mancini"
+
 }
 
 variable "location" {
-    type = string
-    description = "Azure region where the resource group will be created"
-    default = "East US"
-  
+  type        = string
+  description = "Azure region where the resource group will be created"
+  default     = "East US"
+
 }
